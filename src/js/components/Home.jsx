56886cb8c5a0ab80fd6266c6bd681b7c495a620b
Zodiax37 <98854282+Zodiax37@users.noreply.github.com>
2025-05-22 01:36:48 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [tasks, setTasks] = useState([])
+
+
+	let handleEnter = (e) =>{
+		if (e.key === 'Enter') {
+		e.preventDefault(); 
+
+		let task = e.target.value.trim(); 
+		if (task !== "") {
+			setTasks([...tasks, task]); 
+			e.target.value = ""; 
+		}
+	}
+	
+	}
+
+
+const deleteTasks = (i) => {
+		const updt_Tasks = tasks.filter((_, index) => index !== i);
+		setTasks(updt_Tasks);
+	};
+
+	return (
+		<div className="container mt-5 text-center">
+			<h1 className="mb-4">todos</h1>
+			<input
+				type="text" className="form-control" placeholder="Escribe una tarea y presiona Enter" onKeyDown={handleEnter}
+			/>
+
+			<div className="mt-4">
+				{tasks.map((task, index) => (
+					<div key={index} className="d-flex justify-content-between align-items-center mb-2 border p-2 rounded">
+						<span>{task}</span>
+						<button className="btn btn-sm btn-danger" onClick={() => deleteTasks(index)}>X</button>
+					</div>
+				))}
+				<small className="text-muted"> {tasks.length === 0 ? "No tienes tareas pendientes" : `${tasks.length} tarea${tasks.length > 1 ? "s" : ""} pendiente${tasks.length > 1 ? "s" : ""}`}
+				</small>
+			</div>
 		</div>
 	);
 };
